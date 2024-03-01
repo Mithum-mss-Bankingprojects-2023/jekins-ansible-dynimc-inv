bf29175187bac965be0f795f133ca34173110144
mentordimgba <60414886+mentordimgba@users.noreply.github.com>
2024-03-01 06:51:38 -0700

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,17 @@
 provider "aws"{
-region = "ap-south-1"
+region = "us-west-1"
+
 }
 
-
-resource "aws_instance"  "AWSServer"{
-ami = "ami-0e670eb768a5fc3d4"
-instance_type = "t2.micro"
-key_name = "devopsdecbatchmss"
-security_groups = ["launch-wizard-2"]
-tags = {
-Name = "TerraformServer"
+resource "aws_instance" "AWSEC2Instance"{
+ami = "ami-07619059e86eaaaa2"
+Instance_type = "t2.micro"
+key_name = "DemoServer"
+security_groups = ["Demo_server launch-wizard-1"]
+tags={
+Name = "tomcatservers_new"
 }
+
 }
 
 
